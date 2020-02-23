f23659d5f8c2d597f5b0f582b4587f838e567d74
Terry Reynolds <terrancejreynolds@gmail.com>
2020-02-22 23:15:11 -0500

    fixed login ref focus issue using conditionals

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -19,6 +19,8 @@ class Login extends React.Component {
  
     //create a ref for username field on login form
 this.loginInput = React.createRef();
+
+
 this.passwordInput = React.createRef();
     this.state = {
       
@@ -29,38 +31,69 @@ this.passwordInput = React.createRef();
       displayFlashMsg: false,
       msg: '',
       cursorInUser: true
-        
     };
-
+    
     axios.defaults.withCredentials = true;
   }
 
   componentDidMount(){
-    if(this.state.cursorInUser) {
+    console.log('compoenentDidMount Login');
+    let focus = this.loginInput.current;
+    console.log('focus', focus);
+    if(focus) {
+      console.log('doing focus');
       this.loginInput.current.focus();
-    }else {
-this.passwordInput.current.focus();
-    }
-    
-  }
+    }  
+  };
+
   componentDidUpdate() {
+    console.log('componentDidUpdate Login');
+   let focus = this.loginInput.current;
+   let focusPw = this.passwordInput.current;
+   if(focus || focusPw){
     if(this.state.cursorInUser) {
       this.loginInput.current.focus();
-    }else {
+    } else {
       this.passwordInput.current.focus();
     }
-    
+   }
+   
+  };
+
+  toggleInUser = () => {
+    if(this.cursorInUser) {
+      this.setState({
+        cursorInUser: false
+      });
+    } else if(this.cursorInUser){
+      this.setState({
+        cursorInUser: true
+      });
+    }
   }
+
+  componentWillUnmount() {
+    console.log('unmounting');
+  }
+
 
  handleChange = (e) => {
   console.log('e', e.target.name);
    const target = e.target;
    const value = target.value
    const name = target.name;
-
-   this.setState({
-[name]: value
-   });
+if(e.target.name === 'username') {
+  this.setState({
+    [name]: value,
+    cursorInUser: true
+       });
+}else {
+  this.setState({
+    [name]: value,
+    cursorInUser: false
+  });
+}
+ 
  }
 
 
@@ -124,22 +157,17 @@ console.log('completeRes', res.data);
         });
         
         }, 1500);
-//immediately reset cursorInUser to remount
-        // setTimeout(function() {
-      
-        //   self.setState({
-        //   cursorInUser: true
-        // });
-        
-        // }, 1500);
+
       }
       
       
     })
   }
 
+ 
+
   render() {
-    console.log('rendering Login');
+    
     //conditionally rendering the home page
     // const name = this.state.sessionUserName;
     //Also, using a function passed from app.js to update its loggedIn state
@@ -151,6 +179,7 @@ console.log('completeRes', res.data);
     }}
 />
     }
+    console.log('rendering Login');
     return (
         <div> 
             
@@ -158,8 +187,8 @@ console.log('completeRes', res.data);
       <h1 style={buttonStyle}>To-Do Login</h1>
       <h5 className={this.state.displayFlashMsg ? 'displayFlash' : 'hideFlash'}     
       >{this.state.msg}</h5>
-      <input ref={this.loginInput} style= {inputStyle} placeholder='username'  id='username' onChange={this.handleChange} value={this.state.username} name='username' type='text'   />
-        <input ref={this.passwordInput} style= {inputStyle}  placeholder='password ' id='password'  onChange={this.handleChange} value={this.state.password} name='password' type='password'    />
+      <input ref={this.loginInput} style= {inputStyle} placeholder='username'  id='username' onClick={this.toggleInUser} onChange={this.handleChange} value={this.state.username} name='username' type='text'   />
+        <input ref={this.passwordInput} style= {inputStyle}  placeholder='password ' id='password' onClick={this.toggleInUser} onChange={this.handleChange} value={this.state.password} name='password' type='password'    />
         <button style={buttonStyle} type='submit'>Submit</button>
       </form>
       </div>
