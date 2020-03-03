ae4031caf8f0bff47346c7e7037f6b88c7a90b6b
Terry Reynolds <terrancejreynolds@gmail.com>
2020-03-02 22:07:40 -0500

    added 3 lines to dashboard.js to combat FOUC

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,15 +6,23 @@ class Dashboard extends React.Component {
     super(props);
    
     this.state = {
-        
+        ready: false
     };
 
+
+
+  }
+  componentDidMount() {
+    this.setState({
+      ready: true
+    });
   }
 
   render() {
    console.log('rendering Dashboard');
     return (
-        <div className="dashboardTitle"> 
+      //state style change to combat FOUC
+        <div className="dashboardTitle" style={{visibility: this.state.ready ? 'visible' : 'hidden'}}> 
             <h1>The Obligatory ToDo App</h1>
             <h4>Todo Together</h4>      
       </div>
