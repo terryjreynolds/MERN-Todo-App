5aa78147f854a7fb55f690b4002430a9c7c49550
Terry Reynolds <terrancejreynolds@gmail.com>
2020-03-27 15:28:57 -0400

    ?

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ require("dotenv").config();
 //Init app
 const app = express();
 
-const port = process.env.PORT || 5000;
-
 //connect to the database
 mongoose
   .connect(process.env.MONGODB_URL, {
@@ -95,6 +93,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
