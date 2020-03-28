764ff6fbe9e081300d0d018eb2904b9513126fb4
Terry Reynolds <terrancejreynolds@gmail.com>
2020-03-28 10:36:47 -0400

    moved port to top

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 //Init app
 const app = express();
-
+const port = process.env.PORT || 5000;
 //connect to the database
 mongoose
   .connect(process.env.MONGODB_URL, {
@@ -93,7 +93,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
