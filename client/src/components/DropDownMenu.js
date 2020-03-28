5934c19e144728e237b020d7b67c8515e07bbf34
Terry Reynolds <terrancejreynolds@gmail.com>
2020-03-28 18:40:26 -0400

    style changes to accomodate mobile a bit better

--- client/src/components/DropDownMenu.js
+++ client/src/components/DropDownMenu.js
@@ -1,87 +1,83 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import { Redirect } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class DropDownMenu extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       showMenu: false,
-      logoutPath: '',     
+      logoutPath: ""
     };
-    
+
     this.showMenu = this.showMenu.bind(this);
     this.closeMenu = this.closeMenu.bind(this);
   }
-  
+
   showMenu(event) {
-    console.log('inshowMenu');
+    console.log("inshowMenu");
     event.preventDefault();
-    
+
     this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
+      document.addEventListener("click", this.closeMenu);
     });
   }
-  
+
   closeMenu(event) {
-    console.log('incloseMenu');
-    
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });  
-      
-    
+    console.log("incloseMenu");
+
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener("click", this.closeMenu);
+    });
   }
   getAccountProfile = () => {
-    console.log('in getAccountProfile');
-  }
+    console.log("in getAccountProfile");
+  };
 
-  handleLogout = () => { 
-    
-      //hit the logout endpoint
-      axios
+  handleLogout = () => {
+    //hit the logout endpoint
+    axios
       .post(`/users/logout`)
       .then(res => {
         if (res.data) {
-          console.log('redirectobj', res.data.redirect);               
-           this.setState({
-            logoutPath: '/'
-           });
-           
+          console.log("redirectobj", res.data.redirect);
+          this.setState({
+            logoutPath: "/"
+          });
         }
       })
-     
-      .catch(err => console.log(err));
-       
-      };
 
-      handleClick= (e) => {
-        e.preventDefault();
-        console.log('in handleDelete');
-        this.props.setShowModalDeleteState(true);   
-      }
-      
+      .catch(err => console.log(err));
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+    console.log("in handleDelete");
+    this.props.setShowModalDeleteState(true);
+  };
+
   render() {
-    console.log('rendering DropDown');
-    if(this.state.logoutPath === '/') {
-        this.setState({
-          logoutPath: ''
-        });
-        this.props.setUserState(false);
-        this.props.setUrlState('/')
-      return <Redirect to={{
-        pathname: 
-        '/'
-        }}
-        
-    />
+    console.log("rendering DropDown");
+    if (this.state.logoutPath === "/") {
+      this.setState({
+        logoutPath: ""
+      });
+      this.props.setUserState(false);
+      this.props.setUrlState("/");
+      return (
+        <Redirect
+          to={{
+            pathname: "/"
+          }}
+        />
+      );
     }
     const listItemStyle = {
-      display: 'block',
-      fontSize: 'calc(5px + (3 + 8) * ((100vw - 300px) / (1600 - 300)))'
+      display: "block",
+      fontSize: "calc(12px + (3 + 8) * ((100vw - 600px) / (1600 - 300)))",
+      marginBottom: "10px"
     };
 
     return (
@@ -89,31 +85,35 @@ class DropDownMenu extends Component {
         <div className="dropDownButton" onClick={this.showMenu}>
           Account
         </div>
-        
-        {
-          this.state.showMenu
-            ? (
-                <div className="menuWrapper">
-              <ul
-                className="dropDownMenu"
-                ref={(element) => {
-                  this.dropdownMenu = element;
-                }}
-              >
-              <Link style={{textDecoration: 'none'}} to="/profile"><button onClick={this.closeMenu}style={listItemStyle}>Profile</button> </Link>
-              <Link style={{textDecoration: 'none'}} to="/passwordChange"><button onClick={this.closeMenu}style={listItemStyle}>Change Password</button> </Link>
-              <button onClick ={ this.handleClick } style={listItemStyle}> Delete Account</button>
-                <button onClick={ this.handleLogout } style={listItemStyle}>Logout</button>
-                
-               
-              </ul>
-              </div>
-            )
-            : (
-              null
-            )
-        }
-        
+
+        {this.state.showMenu ? (
+          <div className="menuWrapper">
+            <ul
+              className="dropDownMenu"
+              ref={element => {
+                this.dropdownMenu = element;
+              }}
+            >
+              <Link style={{ textDecoration: "none" }} to="/profile">
+                <button onClick={this.closeMenu} style={listItemStyle}>
+                  Profile
+                </button>{" "}
+              </Link>
+              <Link style={{ textDecoration: "none" }} to="/passwordChange">
+                <button onClick={this.closeMenu} style={listItemStyle}>
+                  Change Password
+                </button>{" "}
+              </Link>
+              <button onClick={this.handleClick} style={listItemStyle}>
+                {" "}
+                Delete Account
+              </button>
+              <button onClick={this.handleLogout} style={listItemStyle}>
+                Logout
+              </button>
+            </ul>
+          </div>
+        ) : null}
       </div>
     );
   }
